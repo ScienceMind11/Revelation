cf2f37d1984bfee90de122165cd1c38dc63381ef
ScienceMind11 <sciencemind@icloud.com>
2025-11-22 21:48:18 -0500

    switch to SimpleInventory from List

--- src/main/java/com/sember/revelation/component/entity/AccessoriesComponent.java
+++ src/main/java/com/sember/revelation/component/entity/AccessoriesComponent.java
@@ -2,6 +2,7 @@ package com.sember.revelation.component.entity;
 
 import com.mojang.serialization.Codec;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -25,23 +26,23 @@ public class AccessoriesComponent implements AutoSyncedComponent {
 
     private int slots;
     private int selected;
-    private List<ItemStack> accessories;
+    private SimpleInventory accessories;
 
     public AccessoriesComponent(PlayerEntity provider) {
         this.provider = provider;
         this.slots = BASE_SLOTS;
         this.selected = 0;
-        this.accessories = new ArrayList<>(BASE_SLOTS);
+        this.accessories = new SimpleInventory(BASE_SLOTS);
     }
 
     public ItemStack get(int slot) {
-        if (slot < 0 || slot >= this.slots || slot >= this.accessories.size() || this.accessories.get(slot) == null) return ItemStack.EMPTY;
-        return this.accessories.get(slot);
+        if (slot < 0 || slot >= this.slots || slot >= this.accessories.size() || this.accessories.getStack(slot) == null) return ItemStack.EMPTY;
+        return this.accessories.getStack(slot);
     }
 
     public void set(int slot, ItemStack stack) {
         if (slot < 0 || slot >= this.slots || slot >= this.accessories.size()) return;
-        this.accessories.set(slot, stack);
+        this.accessories.setStack(slot, stack);
     }
 
     public int getSelected() {
@@ -59,21 +60,25 @@ public class AccessoriesComponent implements AutoSyncedComponent {
     }
 
     public boolean isWearing(Item item) {
-        return this.accessories.stream().anyMatch(stack -> stack.getItem() == item);
+        return this.accessories.containsAny(stack -> stack.getItem() == item);
     }
 
     @Override
     public void readData(ReadView view) {
         this.slots = view.getInt("slots", BASE_SLOTS);
         this.selected = view.getInt("selected", 0);
-        this.accessories = view.read("items", codec()).orElse(new ArrayList<>(this.slots));
+        this.accessories = new SimpleInventory(view
+                .read("items", codec())
+                .orElse(new ArrayList<>(5))
+                .toArray(new ItemStack[5])
+        );
     }
 
     @Override
     public void writeData(WriteView view) {
         view.putInt("slots", this.slots);
         view.putInt("selected", this.selected);
-        view.put("items", codec(), this.accessories);
+        view.put("items", codec(), this.accessories.getHeldStacks());
     }
 
     private Codec<List<ItemStack>> codec() {
